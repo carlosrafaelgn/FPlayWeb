5eb24d4bd3f47abe4560bc50094e0e0dd3366547
Carlos Rafael Gimenes das Neves <carlosrafael.prog@gmail.com>
2022-03-21 09:37:10 -0300

    Upgrading the closure compiler

--- tscmin.bat
+++ tscmin.bat
@@ -1,15 +1,16 @@
 @ECHO OFF
 
 REM https://github.com/google/closure-compiler/wiki/Flags-and-Options
+REM We can use --rewrite_polyfills FALSE to remove the polyfills added to the beginning of the output file
 
 REM CALL tsc --target ES2017 --project scripts\tsconfig.json
-REM java -jar D:\Tools\closure-compiler.jar --js docs\assets\js\scripts.js --js_output_file docs\assets\js\scripts.min.js --language_in ECMASCRIPT_2017 --language_out ECMASCRIPT_2017 --strict_mode_input --compilation_level SIMPLE
+REM java -jar D:\Tools\closure-compiler.jar --js docs\assets\js\scripts.js --js_output_file docs\assets\js\scripts.min.js --language_in ECMASCRIPT_2017 --language_out ECMASCRIPT_2017 --strict_mode_input --emit_use_strict --compilation_level SIMPLE
 REM DEL docs\assets\js\scripts.js
 
 REM CALL tsc --target ES2015 --project scripts\tsconfig.json
-REM java -jar D:\Tools\closure-compiler.jar --js docs\assets\js\scripts.js --js_output_file docs\assets\js\scripts.es6.min.js --language_in ECMASCRIPT_2015 --language_out ECMASCRIPT_2015 --strict_mode_input --compilation_level SIMPLE
+REM java -jar D:\Tools\closure-compiler.jar --js docs\assets\js\scripts.js --js_output_file docs\assets\js\scripts.es6.min.js --language_in ECMASCRIPT_2015 --language_out ECMASCRIPT_2015 --strict_mode_input --emit_use_strict --compilation_level SIMPLE
 REM DEL docs\assets\js\scripts.js
 
 CALL tsc --target ES2015 --project scripts\tsconfig.json
-java -jar D:\Tools\closure-compiler.jar --js docs\assets\js\scripts.js --js_output_file docs\assets\js\scripts.min.js --language_in ECMASCRIPT_2015 --language_out ECMASCRIPT_2015 --strict_mode_input --compilation_level SIMPLE
+java -jar D:\Tools\closure-compiler.jar --js docs\assets\js\scripts.js --js_output_file docs\assets\js\scripts.min.js --language_in ECMASCRIPT_2015 --language_out ECMASCRIPT_2015 --strict_mode_input --emit_use_strict --compilation_level SIMPLE
 DEL docs\assets\js\scripts.js
--- tscmin.sh
+++ tscmin.sh
@@ -1,13 +1,14 @@
 # https://github.com/google/closure-compiler/wiki/Flags-and-Options
+# We can use --rewrite_polyfills FALSE to remove the polyfills added to the beginning of the output file
 
 # tsc --target ES2017 --project scripts/tsconfig.json
-# java -jar /d/Tools/closure-compiler.jar --js docs/assets/js/scripts.js --js_output_file docs/assets/js/scripts.min.js --language_in ECMASCRIPT_2017 --language_out ECMASCRIPT_2017 --strict_mode_input --compilation_level SIMPLE
+# java -jar /d/Tools/closure-compiler.jar --js docs/assets/js/scripts.js --js_output_file docs/assets/js/scripts.min.js --language_in ECMASCRIPT_2017 --language_out ECMASCRIPT_2017 --strict_mode_input --emit_use_strict --compilation_level SIMPLE
 # rm docs/assets/js/scripts.js
 
 # tsc --target ES2015 --project scripts/tsconfig.json
-# java -jar /d/Tools/closure-compiler.jar --js docs/assets/js/scripts.js --js_output_file docs/assets/js/scripts.es6.min.js --language_in ECMASCRIPT_2015 --language_out ECMASCRIPT_2015 --strict_mode_input --compilation_level SIMPLE
+# java -jar /d/Tools/closure-compiler.jar --js docs/assets/js/scripts.js --js_output_file docs/assets/js/scripts.es6.min.js --language_in ECMASCRIPT_2015 --language_out ECMASCRIPT_2015 --strict_mode_input --emit_use_strict --compilation_level SIMPLE
 # rm docs/assets/js/scripts.js
 
 tsc --target ES2015 --project scripts/tsconfig.json
-java -jar /d/Tools/closure-compiler.jar --js docs/assets/js/scripts.js --js_output_file docs/assets/js/scripts.min.js --language_in ECMASCRIPT_2015 --language_out ECMASCRIPT_2015 --strict_mode_input --compilation_level SIMPLE
+java -jar /d/Tools/closure-compiler.jar --js docs/assets/js/scripts.js --js_output_file docs/assets/js/scripts.min.js --language_in ECMASCRIPT_2015 --language_out ECMASCRIPT_2015 --strict_mode_input --emit_use_strict --compilation_level SIMPLE
 rm docs/assets/js/scripts.js
